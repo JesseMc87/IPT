cdaca9141664b1406ed9a2f08530ca4ef15bb185
Boyles, Prince Niño L. <94779042+JesseMc87@users.noreply.github.com>
2025-04-08 19:40:58 +0800

    Add files via upload

--- IPT/components/head.php
+++ IPT/components/head.php
@@ -10,6 +10,7 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
         <link href="../styles/index.css" rel="stylesheet" type="text/css">
         <link href="../styles/pagination.css" rel="stylesheet" type="text/css">
+        <title><?php echo isset($title) ? $title : 'Page not found' ?></title>
     </head>
 
 
